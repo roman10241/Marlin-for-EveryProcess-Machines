fcc0754aaa24243f17c5143e0065f5419f7c6f93
Roman <mailbox4rs@yahoo.com>
2017-09-27 20:19:46 +0300

    Z&Y  pins swap
    As RAMPS board has double motor connector on Z axis, it is convenient to
    swap config pins with Y for machine with 2 motors on Y axis

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -95,15 +95,15 @@
 #define X_ENABLE_PIN       38
 #define X_CS_PIN           53
 
-#define Y_STEP_PIN         60
-#define Y_DIR_PIN          61
-#define Y_ENABLE_PIN       56
-#define Y_CS_PIN           49
+#define Y_STEP_PIN         46
+#define Y_DIR_PIN          48
+#define Y_ENABLE_PIN       62
+#define Y_CS_PIN           40
 
-#define Z_STEP_PIN         46
-#define Z_DIR_PIN          48
-#define Z_ENABLE_PIN       62
-#define Z_CS_PIN           40
+#define Z_STEP_PIN         60
+#define Z_DIR_PIN          61
+#define Z_ENABLE_PIN       56
+#define Z_CS_PIN           49
 
 #define E0_STEP_PIN        26
 #define E0_DIR_PIN         28
